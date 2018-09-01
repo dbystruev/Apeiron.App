f1132aa99ce59a5a74aabf5290bf8c35c12f1709
Denis Bystruev <dbystruev@me.com>
2018-09-01 19:11:13 +0300

    Initial Commit

---
+++ Apeiron.App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Apeiron.App
+//
+//  Created by Denis Bystruev on 01/09/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
