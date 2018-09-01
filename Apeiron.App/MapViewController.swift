77340fec5b57d7848c1faba86cf5e7ed511362d0
Denis Bystruev <dbystruev@me.com>
2018-09-01 20:52:56 +0300

    Initial setup of the first map screen

---
+++ Apeiron.App/MapViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  Apeiron.App
+//
+//  Created by Denis Bystruev on 01/09/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    /// Map view presenting the main screen
+    @IBOutlet weak var mapView: MKMapView!
+    
+    /// GPS coordinates for the pins at the map
+    let places = [
+        (55.770811, 37.677652),     // Бауманская улица, 40 (дом, где родился Пушкин)
+        (55.766557, 37.648762),     // Большой Харитоньевский переулок, 21с1 (жили 1802–1803)
+        (55.772679, 37.674993),     // Спартаковская улица, 15 (Собор Богоявления Господня в Елохове)
+        (55.757718, 37.595779),     // Большая Никитская улица, 36с1 (Храм Большое Вознесение)
+        (55.747226, 37.586086),     // улица Арбат, 53с6 (жили 1830–1831)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set initial view of the screen with pins
+        setupMapView()
+    }
+    
+    /// Set initial view of the screen with pins
+    func setupMapView() {
+        
+        /// Performs operation (e. g. min or max) on tuple with two double elements
+        ///
+        /// - Parameters:
+        ///   - left: first tuple
+        ///   - right: second tuple
+        ///   - operation: operation to perform (e. g. min or max)
+        /// - Returns: a tuple each element of which is the result of operation
+        func operate(
+            _ left: (Double, Double),
+            _ right: (Double, Double),
+            operation: (Double, Double) -> Double
+        ) -> (Double, Double) {
+            return (operation(left.0, right.0), operation(left.1, right.1))
+        }
+        
+        // find bottom left and top right coordinates — start with first element
+        var bottomLeft = places.first!
+        var topRight = bottomLeft
+        
+        // go through all places and find bottom left and top right boundaries
+        for place in places {
+            bottomLeft = operate(bottomLeft, place, operation: min)
+            topRight = operate(topRight, place, operation: max)
+        }
+        
+        // find out the center of the map
+        let center = operate(bottomLeft, topRight, operation: { ($0 + $1) / 2 })
+        let centerLocation = CLLocationCoordinate2D(latitude: center.0, longitude: center.1)
+        
+        // find out the maximum delta between the objects on map
+        var delta = operate(bottomLeft, topRight, operation: { 1.5 * abs($0 - $1) })
+        delta.0 = max(delta.0, delta.1)
+        delta.1 = delta.0
+        
+        // allow user to zoom
+        mapView.isZoomEnabled = true
+        
+        // allow user to scroll
+        mapView.isScrollEnabled = true
+        
+        // setup standard map type
+        mapView.mapType = .standard
+        
+        // set the span area
+        let span = MKCoordinateSpan(latitudeDelta: delta.0, longitudeDelta: delta.1)
+        
+        // set the region
+        let region = MKCoordinateRegion(center: centerLocation, span: span)
+        mapView.region = region
+        
+        // set the pins
+        for place in places {
+            setPin(place)
+        }
+    }
+
+    /// Creates a pin on the map at coordinate
+    ///
+    /// - Parameter coordinate: place where to put pin on
+    func setPin(_ coordinate: (Double, Double)) {
+        // create a point annotation
+        let pin = MKPointAnnotation()
+        
+        // set annotation's coordinate
+        pin.coordinate = CLLocationCoordinate2D(latitude: coordinate.0, longitude: coordinate.1)
+        
+        // add annotation to map
+        mapView.addAnnotation(pin)
+    }
+}
+
